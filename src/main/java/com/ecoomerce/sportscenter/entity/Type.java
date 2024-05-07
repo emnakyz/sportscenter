fb560fd30b46b86c65d320dbf57482e46cda126a
Emin Akyüz <emn.ak100@gmail.com>
2024-05-07 14:27:43 +0300

    Type Entity Eklendi

---
+++ src/main/java/com/ecoomerce/sportscenter/entity/Type.java
@@ -0,0 +1,22 @@
+package com.ecoomerce.sportscenter.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="Type")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Type {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="Id")
+    private Integer id;
+    @Column(name="Name")
+    private String name;
+}
